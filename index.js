1001c272f1bb18ab95cf6dcfdcb4d1147113d9ed
Manjeet Saini <manjeetsaini2003.com@gmail.com>
2026-04-14 21:40:26 +0530

    mongodb connected

--- index.js
+++ index.js
@@ -1,11 +1,23 @@
-const express = require('express')
+import express from "express"
+import dotenv from "dotenv"
+import mongoose from "mongoose";
 const app = express()
-const port = 3000
+
+dotenv.config();
+const PORT = process.env.PORT || 3003;
+const URI = process.env.MONGODB_URI;
+try {
+    mongoose.connect(URI)
+    console.log("mongodb connected")
+}
+catch(e){
+    console.log(e);
+}
 
 app.get('/', (req, res) => {
   res.send('Hello World!')
 })
 
-app.listen(port, () => {
-  console.log(`Example app listening on port ${port}`)
+app.listen(PORT, () => {
+  console.log(`Example app listening on port ${PORT}`)
 })
