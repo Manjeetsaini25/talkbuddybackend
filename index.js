b9b5310cafc6c89ea8ecd883c5b05f43bc5d24d4
Manjeet Saini <manjeetsaini2003.com@gmail.com>
2026-04-26 11:55:25 +0530

    Socket.io connected

--- index.js
+++ index.js
@@ -1,47 +1,34 @@
-import express from "express"
-import dotenv from "dotenv"
+import express from "express";
+import dotenv from "dotenv";
 import mongoose from "mongoose";
-import userRoute from "./routes/user.route.js";
-import cors from "cors"
+import cors from "cors";
 import cookieParser from "cookie-parser";
-import messageRoute from './routes/message.route.js'
 
-const app = express()
-
-// app.get('/', (req, res) => {
-//   res.status(200).json({
-//     message: "Server is working"
-//   });
-// });
-
-// Middle Ware
-app.use(express.json());
-app.use(cookieParser())
-app.use(cors());
+import userRoute from "./routes/user.route.js";
+import messageRoute from "./routes/message.route.js";
+import { app, server } from "./SocketIO/server.js";
 
 dotenv.config();
-const PORT = process.env.PORT || 3003;
+
+// middleware
+app.use(express.json());
+app.use(cookieParser());
+app.use(cors());
+
+const PORT = process.env.PORT || 3001;
 const URI = process.env.MONGODB_URI;
+
 try {
-    mongoose.connect(URI)
-    console.log("mongodb connected")
+    mongoose.connect(URI);
+    console.log("Connected to MongoDB");
+} catch (error) {
+    console.log(error);
 }
-catch(e){
-    console.log(e);
-}
 
-//health
-app.get("/health",(req,res)=>{
-    res.status(200).json({
-        "message":"backend s up and running"
-    })
-})
+//routes
+app.use("/api/user", userRoute);
+app.use("/api/message", messageRoute);
 
-
-//Routes
-app.use('/api/user/',userRoute);
-app.use('/api/message',messageRoute);
-
-app.listen(PORT, () => {
-  console.log(`Example app listening on port ${PORT}`)
-})
+server.listen(PORT, () => {
+    console.log(`Server is Running on port ${PORT}`);
+});
